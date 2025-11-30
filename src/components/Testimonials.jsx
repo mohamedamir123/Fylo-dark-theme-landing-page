1dfe337e74cc6adaa944fcbd75d66e350ade0ec9
unknown <mahamedigameing999@gmail.com>
2025-11-30 10:51:51 +0200

    first commit

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,6 +3,7 @@ import TestimonialBox from "./TestimonialBox";
 import Profile1 from "../assets/images/profile-1.jpg"
 import Profile2 from "../assets/images/profile-2.jpg"
 import Profile3 from "../assets/images/profile-3.jpg"
+import Qoute from "../assets/images/bg-quotes.png";
 
 function Testimonials() {
     const [testData, setTestData] = useState([
@@ -29,24 +30,24 @@ function Testimonials() {
       },
     ]);
     return (
-        <section className="pb-[350px] ">
+      <section className="pb-[350px] ">
         <div className="container relative">
-            <div className="absolute left-[20px] top-[-35px]">
-            <img src="/src/assets/images/bg-quotes.png" alt="quote" />
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[40px] relative z-10">
+          <div className="absolute left-[20px] top-[-35px]">
+            <img src={Qoute} alt="quote" />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[40px] relative z-10">
             {testData.map((item) => (
-                <TestimonialBox
+              <TestimonialBox
                 key={item.id}
                 desc={item.desc}
                 image={item.image}
                 position={item.position}
                 name={item.name}
-                />
+              />
             ))}
-            </div>
+          </div>
         </div>
-        </section>
+      </section>
     );
 }
 
